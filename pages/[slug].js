9cf71b91b7bf755fe8cf0496e94818682bfa3320
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2021-07-28 13:40:27 -0500

    Chore: Agregando componentes embebido

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,9 +1,10 @@
 import { MDXRemote } from "next-mdx-remote";
 import { getFileBySlug, getFiles } from "../lib/mdx"
+import  MDXComponents from "../Components/MDXComponents";
 
 export default function Post({source, frontmatter}) {
   console.log(source)
-  return <MDXRemote {...source}  />
+  return <MDXRemote {...source} components={MDXComponents}  />
 }
 export async function getStaticProps({ params }) {
   const { source, frontmatter } = await getFileBySlug("posts", params.slug);
